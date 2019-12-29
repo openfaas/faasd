52baca9d17547b135a54dff65eb4ba1468abb53d
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-12-29 12:00:34 +0000

    Update download location of Go
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- hack/build-containerd.sh
+++ hack/build-containerd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+export ARCH="amd64"
 echo "Downloading Go"
 
-curl -SLsf https://dl.google.com/go/go1.12.14.linux-amd64.tar.gz > go.tgz
+curl -SLsf https://dl.google.com/go/go1.12.14.linux-$ARCH.tar.gz --output /tmp/go.tgz
 sudo rm -rf /usr/local/go/
 sudo mkdir -p /usr/local/go/
-sudo tar -xvf go.tgz -C /usr/local/go/ --strip-components=1
+sudo tar -xvf /tmp/go.tgz -C /usr/local/go/ --strip-components=1
 
 export GOPATH=$HOME/go/
 export PATH=$PATH:/usr/local/go/bin/
