openfaas/faasd

commit 560c295eb0ee760541de9203759936065d71404e

Enable labeling containers
Enabling the faasd-provider to label containers

Signed-off-by: Martin Dekov <mvdekov@gmail.com>

pkg/provider/handlers/deploy.go
@@ -107,6 +107,7 @@ func deploy(ctx context.Context, req types.FunctionDeployment, client *container
 			oci.WithCapabilities([]string{"CAP_NET_RAW"}),
 			oci.WithMounts(mounts),
 			oci.WithEnv(envs)),
+		containerd.WithContainerLabels(*req.Labels),
 	)
 
 	if err != nil {

pkg/provider/handlers/functions.go
@@ -3,10 +3,11 @@ package handlers
 import (
 	"context"
 	"fmt"
+	"log"
 
-	"github.com/openfaas/faasd/pkg/cninetwork"
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/namespaces"
+	"github.com/openfaas/faasd/pkg/cninetwork"
 )
 
 type Function struct {
@@ -16,6 +17,7 @@ type Function struct {
 	pid       uint32
 	replicas  int
 	IP        string
+	labels    map[string]string
 }
 
 const (
@@ -44,10 +46,18 @@ func GetFunction(client *containerd.Client, name string) (Function, error) {
 	if err == nil {
 
 		image, _ := c.Image(ctx)
+
+		containerName := c.ID()
+		labels, labelErr := c.Labels(ctx)
+		if labelErr != nil {
+			log.Printf("cannot list container %s labels: %s", containerName, labelErr.Error())
+		}
+
 		f := Function{
-			name:      c.ID(),
+			name:      containerName,
 			namespace: FunctionNamespace,
 			image:     image.Name(),
+			labels:    labels,
 		}
 
 		replicas := 0

pkg/provider/handlers/read.go
@@ -26,6 +26,7 @@ func MakeReadHandler(client *containerd.Client) func(w http.ResponseWriter, r *h
 				Image:     function.image,
 				Replicas:  uint64(function.replicas),
 				Namespace: function.namespace,
+				Labels:    &function.labels,
 			})
 		}
 

commit 917b65ae1e76c90c2e3b583778e2c059ff2eb796

Add null-checking for labels
Fixes an issue introduced in #45 which was undetected. When
users do not pass in "labels" to the deployment - or a valid
empty object, then a nil dereference causes a panic.

Fixes: #101

Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

pkg/provider/handlers/deploy.go
@@ -69,6 +69,7 @@ func deploy(ctx context.Context, req types.FunctionDeployment, client *container
 	if err != nil {
 		return err
 	}
+
 	imgRef := reference.TagNameOnly(r).String()
 
 	snapshotter := ""
@@ -98,6 +99,11 @@ func deploy(ctx context.Context, req types.FunctionDeployment, client *container
 
 	name := req.Service
 
+	labels := map[string]string{}
+	if req.Labels != nil {
+		labels = *req.Labels
+	}
+
 	container, err := client.NewContainer(
 		ctx,
 		name,
@@ -108,7 +114,7 @@ func deploy(ctx context.Context, req types.FunctionDeployment, client *container
 			oci.WithCapabilities([]string{"CAP_NET_RAW"}),
 			oci.WithMounts(mounts),
 			oci.WithEnv(envs)),
-		containerd.WithContainerLabels(*req.Labels),
+		containerd.WithContainerLabels(labels),
 	)
 
 	if err != nil {