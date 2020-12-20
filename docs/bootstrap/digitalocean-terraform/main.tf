ef639e9b8d6308f2af31eef31386bedc1793e929
Lewis Cowles <lewiscowles@me.com>
2020-12-20 21:16:54 +0000

    Overcome terraform DO missing pinning
    This was all I needed to overcome terraform init failing

--- docs/bootstrap/digitalocean-terraform/main.tf
+++ docs/bootstrap/digitalocean-terraform/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 0.12"
+  
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.3.0"
+    }
+  }
 }
 
 variable "do_token" {
